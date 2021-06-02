5382af794111a7e1834a6c24ff8a38cc17b94aeb
Yogesh Manware <yogesh.manware@gmail.com>
2021-06-02 19:51:58 +1000

    - worked but timed-out for one test case

--- HackerRank/arrays/fraudulentActivity2.js
+++ HackerRank/arrays/fraudulentActivity2.js
@@ -2,27 +2,38 @@
  * Detect fraud given the list of expenditures and number of days
  * https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
  **/
-const lodash = require('lodash');
+
 const data = require('./data.json');
 
 function activityNotifications(expenditure, d) {
     let noticeCount = 0;
     const trail = expenditure.slice(0, d).sort((a, b) => a - b);
-    let time = 0;
     for (let i = d; i < expenditure.length; i++) {
+        console.info(`index: ${i}   notice: ${noticeCount}`)
+        //console.info('trail.length' + trail.length)
         if (expenditure[i] >= findLimit(trail, d)) {
             noticeCount++;
         }
         //delete the exp[i-d] from trail
-        let index = lodash.sortedLastIndex(trail, expenditure[i - d]);
-        trail.splice(index-1, 1);
+        let index = search(trail, expenditure[i - d]);
+        // console.info('deleting from ' + index);
+        trail.splice(index, 1);
 
         //insert exp[i] in trail
-        index = lodash.sortedLastIndex(trail, expenditure[i]);
-        trail.splice(index, 0, expenditure[i]);
+        // console.info('inserting at ' + (index + 1));
+        trail.splice(search(trail, expenditure[i]) + 1, 0, expenditure[i]);
     }
-    console.info('Total time: ' + time);
     return noticeCount;
+}
+
+
+const search = (array, element) => {
+    let low = 0, high = array.length, result;
+    while (result === undefined && low < high) {
+        let m = Math.floor(low + (high - low) / 2);
+        array[m] === element ? result = m : array[m] < element ? low = m + 1 : high = m - 1;
+    }
+    return result || low;
 }
 
 
@@ -37,7 +48,5 @@ const findLimit = (arr, d) => {
 }
 
 //console.info(activityNotifications([1, 2, 3, 4, 4], 4));//0
-//console.info(activityNotifications([2, 3, 4, 2, 3, 80, 8, 40, 5, 8, 50], 4));//2
-const start = new Date().getTime();
+//console.info(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5, 2, 3, 4, 2, 3, 6, 8, 4, 5], 5));//2
 console.info(activityNotifications(data, 10000));
-console.info('Duration: ' + (new Date().getTime() - start));
